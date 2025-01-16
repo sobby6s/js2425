d70fd842965ff9cfaea40997f0e6ffb95a35a322
Sobolanul <opreastefan92@gmailcom>
2025-01-16 21:05:08 +0200

    acasa

---
+++ curs4/app_initial.js
@@ -0,0 +1,36 @@
+
+console.log(movies);
+
+const main = document.getElementById("main");
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+showMovies(movies, searchTerm='');
+function showMovies(results, searchTerm='') {
+    if(searchTerm !==''){
+        alert(searchTerm);
+        results = results.filter(function (element){
+            return (element.title.toLowerCase().indexOf(searchTerm) > -1);
+        });
+    }
+    results.forEach(element => {
+        const el = document.createElement('div');
+        const image = document.createElement('img');
+        const text = document.createElement('h2');
+
+        text.innerHTML = `${element.title}`;
+        image.src = IMGPATH + element.poster_path;
+        el.appendChild(image);
+        el.appendChild(text);
+        main.appendChild(el);
+    });
+}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+    const searchTerm = search.value;
+    if (searchTerm) {
+        showMovies(movies,
+            searchTerm.toLowerCase()
+        );
+        search.value = "";
+    }
+});
